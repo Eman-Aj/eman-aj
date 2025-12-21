8c06b6fa5a81a1b482d9cbdc25e0dd59d3470153
Eman-Aj <emansawesomejourney@gmail.com>
2025-12-20 23:43:35 -0500

    Minor Fix
    B -> b

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import Navbar from './components/NavBar'
+import Navbar from './components/Navbar'
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
